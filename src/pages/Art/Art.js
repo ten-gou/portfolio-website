78113d37650f17de535e8135d8c5b48fbb42fb00
Maribel <lwang79@zoho.com>
2022-09-25 13:55:56 -0700

    created base and added some urls of code examples

--- src/pages/Art/Art.js
+++ src/pages/Art/Art.js
@@ -6,12 +6,32 @@ import { CssBaseline, Box, Grid, Stack, Button, Typography } from "@mui/material
 const ImageList = () => {
     const list = [
         {
-
+            url: 'https://ten-gou.github.io/Password-Generator/',
+            github: 'https://github.com/ten-gou/Password-Generator',
+            title: 'Password Generator',
+            desc: 'A Javascript based website that allows the user to generate passwords using parameters',
         },
         {
-
+            url: 'https://github.com/ten-gou/readme-generator',
+            github: 'https://github.com/ten-gou/readme-generator',
+            title: 'README Generator',
+            desc: 'A terminal level program which builds README files based on user inputs',
+        },
+        {
+            url: 'https://ten-gou.github.io/Weather6-Dashboard/',
+            github: 'https://github.com/ten-gou/Weather6-Dashboard',
+            title: 'Weather6 Dashboard',
+            desc: 'A weather tracker based on OpenWeather API which can search and log city weather data to local storage',
+        },
+        {
+            url: '',
+            github: '',
+            title: 'Web Portfolio',
+            desc: 'This Website! Built with React and MUI.',
         }
     ]
+
+    return
 }
 
 const Art = () => {
--- src/pages/Code/Code.js
+++ src/pages/Code/Code.js
@@ -1,5 +1,14 @@
 import { Typography } from "@mui/material"
 
+const CodeList = () => {
+    const list = [
+        {
+            'name': '',
+        },
+
+    ]
+}
+
 const Code = () => {
     return (
         <Typography>Code</Typography>
