6167dde041189afc87dda0dea3be4c57961c1109
Maribel <lwang79@zoho.com>
2022-09-27 18:58:19 -0700

    started work on the about me page

---
+++ src/pages/AboutMe/AboutMe.js
@@ -0,0 +1,14 @@
+import { Typography } from "@mui/material";
+
+
+const AboutMe = () => {
+
+
+    return (
+        <>
+        <Typography variant={'h2'}>About Me</Typography>
+        </>
+    )
+}
+
+export default AboutMe;
