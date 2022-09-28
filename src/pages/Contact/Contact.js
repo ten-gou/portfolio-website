90df37c436a1add6f8a069f2eb4cd908067b4935
Maribel <lwang79@zoho.com>
2022-09-28 13:05:50 -0700

    chose to remove some code due to choice in style

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -5,51 +5,32 @@ import { link } from './styles';
 const Contact = () => {
     const theme = useTheme();
 
-    const match = useMediaQuery(theme.breakpoints.up('sm'));
-
     const Link = () => {
-    if (match == false) {
         return  <>
         <Stack>
-            <Button sx={link}>Github<LaunchIcon /></Button>
-            <Button sx={link}>Github<LaunchIcon /></Button>
-            <Button sx={link}>Github<LaunchIcon /></Button>
+            <Button sx={link} href='https://github.com/ten-gou'>Github<LaunchIcon /></Button>
+            <Button sx={link}>Placeholder<LaunchIcon /></Button>
+            <Button sx={link}>Placeholder<LaunchIcon /></Button>
         </Stack>
         </>
-    }
-    else {
-        return <>
-        <Stack flexDirection={'row'}>
-        <Button sx={link}>Github<LaunchIcon /></Button>
-        <Button sx={link}>Github<LaunchIcon /></Button>
-        <Button sx={link}>Github<LaunchIcon /></Button>
-        </Stack>
-        </>
-    }
     }
 
     return (
-        <Box marginTop={40}>
-            <Grid container>
-                <Grid item xs={3} />
-                <Grid item xs={6}>
-                    <Stack>
-                        <Typography variant={'h2'} textAlign={'center'}>Contact</Typography>
-                        <Typography variant={'p'} textAlign={'center'}>Need to get in touch? Use the points of contact listed below:</Typography>
-                    </Stack>
-                </Grid>
-                <Grid item xs={3} />
+        <Grid container marginTop={40} justifyContent={'center'} rowGap={8}>
+            <Grid display={'flex'} flexDirection={'column'}>
+                <Typography variant={'h2'} textAlign={'center'}>Contact</Typography>
+                <Typography variant={'p'} textAlign={'center'}>Need to get in touch? Use the points of contact listed below:</Typography>
             </Grid>
 
-            <Grid container>
-                <Grid item xs={3} sm={5} />
-                <Grid item xs={6} sm={3}>
+            <Grid container display={'flex'}>
+                <Grid item xs={3} sm={4} />
+                <Grid item xs={6} sm={4}>
                     <Link />
                 </Grid>
                 <Grid item xs={3} sm={4}/>
             </Grid>
             
-        </Box>
+        </Grid>
     )
 }
 
