6782e657fcd7d22897981e3d1126b3e86272de2a
Maribel <lwang79@zoho.com>
2022-09-29 17:33:39 -0700

    work complete on the contact page, and a little tinkering in the art page

--- src/pages/Art/Art.js
+++ src/pages/Art/Art.js
@@ -1,30 +1,25 @@
 /* Image List quilted with title bars */
 import * as React from "react";
-
-import { CssBaseline, Box, Grid, Stack, Button, Typography, ImageList, ImageListItem } from "@mui/material";
-import LaunchIcon from '@mui/icons-material/Launch';
+import { Box, ImageList, ImageListItem } from "@mui/material";
 
 const srcset = (image, size, rows = 1, cols = 1) => {
-    return {}
+    return {    
+    src: `${image}?w=${size * cols}&h=${size * rows}&fit=crop&auto=format`,
+    srcSet: `${image}?w=${size * cols}&h=${
+      size * rows
+    }&fit=crop&auto=format&dpr=2 2x`,
+};
 }
 
-const Image = () => {
-    const list = [
-        {
-
-        },
-        {
-
-        },
-        {
-
-        },
-    ]
+const Art = () => {
 
     return (
         <Box>
             <ImageList
-            variant={'quilted'}>
+            sx={{ width: 500, height: 450 }}
+            variant={'quilted'}
+            cols={4}
+            rowHeight={121}>
             {list.map((item, index) => {
                 <ImageListItem key={item.img} cols={item.cols || 1} rows={item.rows || 1}>
                 <img
@@ -32,20 +27,72 @@ const Image = () => {
                   alt={item.title}
                   loading="lazy"
                 />
-            </ImageListItem>
+                </ImageListItem>
             })}
             </ImageList>
         </Box>
     )
 }
 
-const Art = () => {
-
-    return (
-        <>
-        <Image />
-        </>
-    )
-}
+const list = [
+    {
+        img: 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e',
+        title: 'Breakfast',
+        rows: 2,
+        cols: 2,
+      },
+      {
+        img: 'https://images.unsplash.com/photo-1551782450-a2132b4ba21d',
+        title: 'Burger',
+      },
+      {
+        img: 'https://images.unsplash.com/photo-1522770179533-24471fcdba45',
+        title: 'Camera',
+      },
+      {
+        img: 'https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c',
+        title: 'Coffee',
+        cols: 2,
+      },
+      {
+        img: 'https://images.unsplash.com/photo-1533827432537-70133748f5c8',
+        title: 'Hats',
+        cols: 2,
+      },
+      {
+        img: 'https://images.unsplash.com/photo-1558642452-9d2a7deb7f62',
+        title: 'Honey',
+        author: '@arwinneil',
+        rows: 2,
+        cols: 2,
+      },
+      {
+        img: 'https://images.unsplash.com/photo-1516802273409-68526ee1bdd6',
+        title: 'Basketball',
+      },
+      {
+        img: 'https://images.unsplash.com/photo-1518756131217-31eb79b20e8f',
+        title: 'Fern',
+      },
+      {
+        img: 'https://images.unsplash.com/photo-1597645587822-e99fa5d45d25',
+        title: 'Mushrooms',
+        rows: 2,
+        cols: 2,
+      },
+      {
+        img: 'https://images.unsplash.com/photo-1567306301408-9b74779a11af',
+        title: 'Tomato basil',
+      },
+      {
+        img: 'https://images.unsplash.com/photo-1471357674240-e1a485acb3e1',
+        title: 'Sea star',
+      },
+      {
+        img: 'https://images.unsplash.com/photo-1589118949245-7d38baf380d6',
+        title: 'Bike',
+        cols: 2,
+      },
+]
 
 export default Art;
